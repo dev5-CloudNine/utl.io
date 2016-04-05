dfbd77cc926d878950e3b8a69497350e7623328c
prathviraj080 <prathviraj080@gmail.com>
2016-04-05 20:49:17 +0530

    removing stripe #4

--- both/lib/methods.js
+++ both/lib/methods.js
@@ -73,25 +73,25 @@ Meteor.methods({
         	throw new Meteor.Error("You can only pay for you own job post.");
 
         if(Meteor.isServer){
-        	var result = Stripe.charges.create({
-	        	source:tokenId,
-	        	amount:10000,
-	        	currency:"usd",
-	        	description:"We Work Meteor - Featured Job Post - 30 Days"
-	        });
+        	// var result = Stripe.charges.create({
+	        // 	source:tokenId,
+	        // 	amount:10000,
+	        // 	currency:"usd",
+	        // 	description:"We Work Meteor - Featured Job Post - 30 Days"
+	        // });
 
-	        if(result && result.status === "succeeded"){
-	        	Jobs.update({_id:job._id},{
-	        		$set:{
-	        			featuredThrough:moment().add(30,"days").toDate()
-	        		},
-	        		$push:{
-	        			featuredChargeHistory:result.id
-	        		}
-	        	});
-	        }else{
-	        	throw new Meteor.Error("Payment Failed!");
-	        }
+	        // if(result && result.status === "succeeded"){
+	        // 	Jobs.update({_id:job._id},{
+	        // 		$set:{
+	        // 			featuredThrough:moment().add(30,"days").toDate()
+	        // 		},
+	        // 		$push:{
+	        // 			featuredChargeHistory:result.id
+	        // 		}
+	        // 	});
+	        // }else{
+	        // 	throw new Meteor.Error("Payment Failed!");
+	        // }
         }else{
         	Jobs.update({
 	            _id: jobId
--- server/lib/environment.js
+++
@@ -1,5 +0,0 @@
-if (Meteor.settings && Meteor.settings.stripe && Meteor.settings.stripe.secretKey) {
-    Stripe = StripeSync(Meteor.settings.stripe.secretKey);
-}else{
-	console.log('ERROR - Stripe API Key Not Found');
-}
