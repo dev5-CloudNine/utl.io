346f87fc9617fa0865842e23ed1397061e2cedf5
yashosagar <yashosagar@encierrosoftware.com>
2016-04-07 20:11:00 +0530

    Counter offer form

--- client/views/jobs/job.js
+++ client/views/jobs/job.js
@@ -13,6 +13,87 @@ Template.job.events({
   			toastr.success("You've successfully applied for this job!");
   		}
   	})
+  },
+  'change #counter_type': function(event, template) {
+    var counterType = $('#counter_type').val();
+    if(counterType == 'fixed_pay') {
+      $('#fixed-pay-counter').show();
+    }
+    else {
+      $('#fixed-pay-counter').hide();
+    }
+    if(counterType == 'per_hour') {
+      $('#hourly-counter').show();
+    }
+    else {
+      $('#hourly-counter').hide();
+    }
+    if(counterType == 'per_device') {
+      $('#per-device-counter').show();
+    }
+    else {
+      $('#per-device-counter').hide();
+    }
+    if(counterType == 'blended') {
+      $('#blended-counter').show();
+    }
+    else {
+      $('#blended-counter').hide();
+    }
+  },
+  'change #fixed_amount, keyup #fixed_amount': function(event, template) {
+    var fixedamount = $('#fixed_amount').val();
+    $('#total_amount').val(fixedamount);
+    $('#buyer_cost').val(fixedamount);
+    var freenet = fixedamount - (fixedamount * 5/100);
+    $('#freelancer_nets').val(freenet);
+  },
+  'change #hourly_rate, keyup #hourly_rate, change #max_hours, keyup #max_hours': function(event, template) {
+    var hourlyrate = $('#hourly_rate').val();
+    var maxhours = $('#max_hours').val();
+    var totalamount = hourlyrate * maxhours;
+    $('#total_amount').val(totalamount);
+    $('#buyer_cost').val(totalamount)
+    var freenet = totalamount - (totalamount * 5/100);
+    $('#freelancer_nets').val(freenet);
+  },
+  'change #device_rate, keyup #device_rate, change max_devices, keyup max_devices': function(event, template) {
+    var devicerate = $('#device_rate').val();
+    var maxdevices = $('#max_devices').val();
+    var totalamount = devicerate * maxdevices;
+    $('#total_amount').val(totalamount);
+    $('#buyer_cost').val(totalamount)
+    var freenet = totalamount - (totalamount * 5/100);
+    $('#freelancer_nets').val(freenet);
+  },
+  'change #first_hours, keyup #first_hours, change #first_max_hours, keyup #first_max_hours, change #next_hours, keyup #next_hours, change #next_max_hours, keyup #next_max_hours': function(event, template) {
+    var payforfirsthours = $('#first_hours').val();
+    var firsthours = $('#first_max_hours').val();
+    var payfornexthours = $('#next_hours').val();
+    var nexthours = $('#next_max_hours').val();
+    var totalforfirsthours = payforfirsthours * firsthours;
+    var totalfornexthours = payfornexthours * nexthours;
+    var totalamount = totalforfirsthours + totalfornexthours;
+    $('#total_amount').val(totalamount);
+    $('#buyer_cost').val(totalamount)
+    var freenet = totalamount - (totalamount * 5/100);
+    $('#freelancer_nets').val(freenet);
+  },
+  'change input[value="provider"]': function(event, template) {
+    var totalamount = parseFloat($('#total_amount').val());
+    $('#buyer_cost').val(totalamount);
+    var freenet = totalamount - totalamount * 5/100;
+    $('#freelancer_nets').val(freenet);
+  },
+  'change input[value="buyer"]': function(event, template) {
+    var totalamount = parseFloat($('#total_amount').val());
+    var clientCost = totalamount + totalamount * 5/100;
+    $('#buyer_cost').val(clientCost);
+    $('#freelancer_nets').val(totalamount);
+  },
+  'click .counterOffer': function(event, template) {
+    var counterType = $('#counter_type').val();
+    console.log(counterType);
   }
 });
 
@@ -29,6 +110,9 @@ Template.job.helpers({
   },
   'counterOfferCount': function() {
     var count = 0;
+    Jobs.findOne(this._id).counterOffers.forEach(function(uId) {
+      count++;
+    });
     return count;
   },
   'appliedProviders': function() {
@@ -42,6 +126,9 @@ Template.job.helpers({
     });
     return providersApplied;
   },
+  'counterOfferedProviders': function() {
+
+  },
   'jobPostedBuyer': function() {
     var jobDetails = Jobs.findOne(this._id);
     if(jobDetails.userId == Meteor.userId())
@@ -50,4 +137,3 @@ Template.job.helpers({
       return false;
   }
 });
-
