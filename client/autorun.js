59fddc2d756c8dfbd096cbf60096976d6de3c18f
yashosagar <yashosagar@encierrosoftware.com>
2016-06-25 20:47:19 +0530

    cosmetic changes

--- client/autorun.js
+++ client/autorun.js
@@ -8,6 +8,23 @@ Tracker.autorun(function() {
   $(window).on('beforeunload', function() {
   	socket.close();
   })
+  toastr.options = {
+	"closeButton": false,
+	"debug": false,
+	"newestOnTop": false,
+	"progressBar": false,
+	"positionClass": "toast-top-full-width",
+	"preventDuplicates": false,
+	"onclick": null,
+	"showDuration": "300",
+	"hideDuration": "1000",
+	"timeOut": "5000",
+	"extendedTimeOut": "1000",
+	"showEasing": "swing",
+	"hideEasing": "linear",
+	"showMethod": "fadeIn",
+	"hideMethod": "fadeOut"
+}
 });
 
 
